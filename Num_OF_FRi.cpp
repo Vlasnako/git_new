1023fe137e8bf5c17ea5169d83f1806a218ca549
Vlas Nakonechnyi <vlas.nak.05@gmail.com>
2022-12-26 01:26:47 +0200

    fixed a bug

---
+++ Num_OF_FRi.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>nul");
+    cout << "Number of Friday the thirteenths in XX century" << endl;
+    int arr[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    int days = 13, n_fri = 1, n_yrs = 0;
+    for (int i = 4; i < 12; i++)
+    {
+        if (days % 7 == 0)
+        {
+            n_fri++;
+
+        }
+
+        days = days + arr[i];
+    }
+    for (n_yrs = 0; n_yrs < 99; n_yrs++)
+    {
+
+        if (n_yrs % 4 == 0)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (days % 7 == 0)
+                {
+                    n_fri++;
+                }
+                days = days + arr[i] + 1;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                if (days % 7 == 0)
+                {
+                    n_fri++;
+                }
+
+
+                days = days + arr[i];
+            }
+        }
+
+    }
+    cout << "Number: " << n_fri;
+}
+
+
